6ac3dc4a7426b09a13b85822e26fe9bfade27bd7
liushaoming <liushaoming@minshenglife.com>
2018-01-19 09:15:53 +0800

    consistent-hash package

---
+++ flycache-core/src/main/java/io/github/flylib/flycache/app/FlycacheCoreApp.java
@@ -0,0 +1,13 @@
+package io.github.flylib.flycache.app;
+
+/**
+ * @author liushaoming
+ * @Package io.github.flylib.flycache.app
+ * @Description:
+ * @date 2018-1-19 9:14
+ */
+public class FlycacheCoreApp {
+    public static void main(String[] args) {
+        System.out.println("Hello FlycacheCoreApp-----");
+    }
+}
